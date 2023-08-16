36c04e7d7b0a5ec0fd98ddef7f8a3b2a7b6ab5db
noda <nodayoshi@gmail.com>
2023-08-17 00:41:31 +0900

    add en.json

--- www/src/main.js
+++ www/src/main.js
@@ -5,16 +5,21 @@ import { router } from "./router";
 import store from "./store/index";
 import { createI18n } from "vue-i18n";
 import ja from "./language/ja.json";
+import en from "./language/en.json";
 
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
 
 const i18n = createI18n({
   locale: "ja",
+  fallbackLocale: "en",
   messages: {
     ja,
+    en,
   },
 });
+
+console.log(i18n);
 
 dayjs.locale("ja");
 const app = createApp(App).use(router).use(store).use(i18n).provide("dayjs", dayjs);
